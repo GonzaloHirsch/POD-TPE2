ddb545212acaf4530068c908518c690d5893acd3
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-10-27 15:32:27 -0300

    Minor correction to debug script

--- client/src/main/assembly/overlay/run-all.sh
+++ client/src/main/assembly/overlay/run-all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-./query1.sh -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
-./query1.sh -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
+./query1 -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
+./query1 -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
 
-./query2.sh -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=300
-./query2.sh -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=300
+./query2 -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=300
+./query2 -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=300
 
-./query3.sh -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dn=3
-./query3.sh -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dn=3
+./query3 -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dn=3
+./query3 -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dn=3
 
-./query4.sh -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=11000 -Dname='Fraxinus pennsylvanica'
-./query4.sh -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=11000 -Dname='COMMON HORSECHESTNUT'
+./query4 -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=11000 -Dname='Fraxinus pennsylvanica'
+./query4 -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/ -Dmin=11000 -Dname='COMMON HORSECHESTNUT'
 
-./query5.sh -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
-./query5.sh -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
+./query5 -Dcity=BUE "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
+./query5 -Dcity=VAN "$@" -DinPath=./../../../examples/ -DoutPath=./../../../examples/
